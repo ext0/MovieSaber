c4ba8b20b361894c2207d127d87dc7f64dca6873
Patrick Bell <patrick.bell@g.austincc.edu>
2018-08-27 15:19:55 -0500

    Add project files.

---
+++ MovieSaber/MoviePlugin.cs
@@ -0,0 +1,62 @@
+﻿using IllusionPlugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
+namespace MovieSaber
+{
+    public class MoviePlugin : IPlugin
+    {
+        public String Name => "MovieSaber";
+        public String Version => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        public static MoviePlugin Instance => _instance;
+
+        private static MoviePlugin _instance;
+
+        public MoviePlugin()
+        {
+            _instance = this;
+        }
+
+        public void OnApplicationQuit()
+        {
+
+        }
+
+        public void OnApplicationStart()
+        {
+            Output.Logger.Info("Starting plugin...");
+
+            SceneManager.sceneLoaded += (scene, loadSceneMode) =>
+            {
+                Output.Logger.Info("Loaded scene: " + scene.name);
+            };
+        }
+
+        public void OnFixedUpdate()
+        {
+
+        }
+
+        public void OnLevelWasInitialized(int level)
+        {
+
+        }
+
+        public void OnLevelWasLoaded(int level)
+        {
+
+        }
+
+        public void OnUpdate()
+        {
+
+        }
+    }
+}
---
+++ MovieSaber/Output/Logger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieSaber.Output
+{
+    public static class Logger
+    {
+        private static void WriteLine(LogLevel level, String output)
+        {
+            Console.WriteLine(String.Format(
+            "[{0}] [{1}] {2:G} - {3}", MoviePlugin.Instance.Name, level.ToString(), DateTime.Now, output));
+        }
+
+        public static void Debug(String output)
+        {
+            WriteLine(LogLevel.DEBUG, output);
+        }
+
+        public static void Info(String output)
+        {
+            WriteLine(LogLevel.INFO, output);
+        }
+
+        public static void Warn(String output)
+        {
+            WriteLine(LogLevel.WARN, output);
+        }
+        public static void Error(String output)
+        {
+            WriteLine(LogLevel.ERROR, output);
+        }
+
+        private enum LogLevel
+        {
+            DEBUG,
+            INFO,
+            WARN,
+            ERROR
+        }
+    }
+}
